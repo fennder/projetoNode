496208035a4f787349b71544580b0071811a1451
Edir P Santos <fennder@gmail.com>
2025-02-26 10:53:00 -0300

    Update file App.js and inde.css, Created file Frase.midule.css

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from "./Frase.module.css";
+
 function Frase(){
     return(
-        <div>
-            <p>Este componente é apenas para uma Frase teste!</p>
+        <div className="styles.fraseContainer">
+            <p className="styles.fraseContent">Este componente é apenas para uma Frase teste!</p>
         </div>
     );
 }
