746ecfe4c4246c7c03e2e74ff52d8db1fd99ab1c
Edir P Santos <fennder@gmail.com>
2025-02-26 11:14:17 -0300

    Update file App.js, Created components Item and List

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HelloWorld from "./components/HelloWorld";
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import List from './components/List';
 
 function App() {
   return(
@@ -19,6 +20,7 @@ function App() {
         profissao="Programador"
         foto="http://abrindoportas.tec.br/assets/perfil.png"
       />
+      <List />
     </div>
   );
 
---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import Item from './Item'
+
+function List(){
+    return(
+        <div>
+            <h1>Minha Lista de Marcas:</h1>
+            <ul>
+                <li marca="Ferrari" />
+                <li marca="Fiat" />
+                <li marca="Honda" />
+            </ul>
+        </div>
+    );
+}
+
+export default List
