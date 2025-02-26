ce7d1402d90b87203299d7a572cf2e263aff633a
Edir P Santos <fennder@gmail.com>
2025-02-26 09:25:06 -0300

    Update file App.js, criei uma função soma e imprimi no html

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Alteração no arquivo<code>src/App.js</code> e salve para atualizar.
-        </p>
-        <p>
-          Segundo texto para visualização.
-        </p>
-        <a
-          className="App-link"
-          href="https://abrindoportas.tec.br"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vá para o Abrindo Portas Tecnologia
-        </a>
-      </header>
+  const name = "Edir Pereira";
+  let a = 1;
+  let b = 2;
+  
+  function sum(){
+    return a + b;
+  }
+
+  const url = "https://upload.wikimedia.org/wikipedia/pt/4/44/GladiadorPoster.jpg";
+
+  return(
+    <div className='App'>
+      <p>Testando o JSX</p>
+      <p>Olá {name}</p>
+      <p>Soma é {sum()}</p>
+      <img src='{url}' alt='Informação da Imagem'></img>
     </div>
   );
+
 }
 
 export default App;
